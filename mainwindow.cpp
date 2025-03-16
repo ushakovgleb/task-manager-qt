68d882f64305c7c308ffaa012b2a89e0c4cdc4f9
Gleb Ushakov <um.glebushakov@gmail.com>
2025-03-16 22:22:38 +0300

    добавлена инициализация модели таблицы, добавление задач

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,12 +19,26 @@ MainWindow::MainWindow(QWidget *parent)
     }
 
     query = new QSqlQuery(db);
-    query->exec("CREATE TABLE UsersTasks(Задача TEXT, Заметка TEXT);");
+    query->exec("CREATE TABLE Tasks(Задачи TEXT);");
 
     //инициализация модели
     model = new QSqlTableModel(this, db);
-    model->setTable("UsersTasks");
+    model->setTable("Tasks");
     model->select();
+
+    ui->tableView->setModel(model);
+    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableView->setStyleSheet(
+            "QTableView::item {"
+            "    border: 1px solid #000000;"
+            "    padding: 5px;"
+            "}"
+            "QHeaderView::section {"
+            "    background-color: #f0f0f0;"
+            "    padding: 4px;"
+            "    border: 1px solid #dcdcdc;"
+            "}"
+        );
 
 }
 
@@ -32,3 +46,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    model->insertRow(model->rowCount());
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    model->insertRow(model->rowCount());
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -21,6 +21,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::MainWindow *ui;
     QSqlDatabase db;
